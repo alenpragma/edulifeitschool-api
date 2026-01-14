783573f564788d133cdb3a8e23dd21e7ebbec9a3
ovidas <designerovi00@gmail.com>
2026-01-14 15:10:43 +0600

    updated

--- src/modules/public/public.service.ts
+++ src/modules/public/public.service.ts
@@ -22,7 +22,7 @@ export const getSiteSettings = async () => {
           s.key,
           {
             ...s.value,
-            heroImage: path.join(config.BASE_URL, s.value.heroImage),
+            heroImage: `${config.BASE_URL}${s.value.heroImage}`,
           },
         ];
       }
