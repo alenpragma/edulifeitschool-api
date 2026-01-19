4f5fd5b0bccab10d98a7367ea9b1d03d86c3b023
ovidas <designerovi00@gmail.com>
2026-01-19 16:08:15 +0600

    updated

--- src/modules/site-setting/site-setting.route.ts
+++ src/modules/site-setting/site-setting.route.ts
@@ -4,35 +4,25 @@ import * as siteSettingController from "./site-setting.controller";
 import { createDynamicUploader } from "../../middlewares/multer.middleware";
 
 const router = Router();
-
 const upload = createDynamicUploader((field) => {
   switch (field) {
     case "heroImage":
       return { folder: "site-settings", filename: "hero-image" };
-    case "bannerImage": // WhyChooseUs
+    case "bannerImage":
       return { folder: "site-settings", filename: "why-choose-us-banner" };
     default:
       return { folder: "others" };
   }
 });
 
-// Upsert Hero
 router.post(
-  "/hero",
+  "/",
   upload.single("heroImage"),
-  siteSettingValidator.validateUpsert,
-  siteSettingController.upsertSiteSettingController,
-);
-
-// Upsert WhyChooseUs
-router.post(
-  "/why-choose-us",
   upload.single("bannerImage"),
   siteSettingValidator.validateUpsert,
   siteSettingController.upsertSiteSettingController,
 );
 
-// Get all site settings
 router.get("/", siteSettingController.getSiteSettingsController);
 
 export default router;
